6a86c62746eddf9194cfff50c82107f1535649f7
Himani.Vaidya <85447801+hvaidya14@users.noreply.github.com>
2025-03-14 15:12:32 -0700

    Update sample.java

--- sample.java
+++ sample.java
@@ -5,3 +5,51 @@
 
 
 // Your code here along with comments explaining your approach
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    public Node cloneGraph(Node node) {
+        if(node == null) {
+            return null;
+        }
+        Map<Node, Node> m = new HashMap<>();
+        Queue<Node> q = new LinkedList<>();
+        Node node1 = new Node(node.val);
+        m.put(node, node1);
+        q.add(node);
+        while(!q.isEmpty()) {
+            Node existing = q.poll();
+            List<Node> neighbors = existing.neighbors;
+            for (Node neighbor:neighbors) {
+                if (!m.containsKey(neighbor)){
+                    Node copynode = new Node(neighbor.val);
+                    m.put(neighbor, copynode);
+                    m.get(existing).neighbors.add(copynode);
+                    q.add(neighbor);
+                } else {
+                    Node copiednode = m.get(neighbor);
+                    m.get(existing).neighbors.add(copiednode);
+                }
+            }
+        }
+        return node1;
+    }
+}
